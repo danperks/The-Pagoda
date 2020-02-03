d282e59d25733895366f917b471bac2719eaabae
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-02-03 14:06:56 +0000

    Added .exex

--- Week 1 - Messaging System/C++/original.cpp
+++ Week 1 - Messaging System/C++/original.cpp
@@ -1,7 +1,7 @@
 #define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
 #include <winsock2.h>
+#include <windows.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,7 +9,6 @@
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
-
 
 
 #define DEFAULT_BUFLEN 512
