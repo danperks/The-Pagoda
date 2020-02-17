093aa9af8878c1db914c6bd935d646390a7ac19b
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-02-17 13:15:04 +0000

    New server code

--- src/Server/C++/Old - 1 Client Working/client.cpp
+++ src/Server/C++/Old - 1 Client Working/client.cpp
@@ -1,82 +1,140 @@
-#define _WIN32_WINNT 0x501 
 #define WIN32_LEAN_AND_MEAN
-#include <stdio.h> // I/O buffer
+
+#include <iostream>
 #include <winsock2.h>
-#include <iostream> // For cout
+#include <windows.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
 
 // needs -lws2_32 in g++ compile statement
-#define DEFAULT_PORT "27015"
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
-//#pragma comment (lib, "Mswsock.lib")
-//#pragma comment (lib, "AdvApi32.lib")
 
-int main(int argc, char **argv) {
-    // based of ms example
-    std::cout<<"it ran"<<std::endl;
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "54000"
+
+int __cdecl main(int argc, char **argv) 
+{
     WSADATA wsaData;
-    struct addrinfo * result = NULL, *ptr = NULL, protocol;
-    SOCKET ourSocket;
-    int WSAResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (WSAResult != 0) {
-        printf("Socket failed");
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    int recvbuflen = DEFAULT_BUFLEN;
+    
+    // Validate the parameters
+    if (argc != 2) {
+        printf("usage: %s server-name\n", argv[0]);
         return 1;
     }
-    protocol.ai_family = AF_UNSPEC;
-    protocol.ai_socktype = SOCK_STREAM;
-    protocol.ai_protocol = IPPROTO_TCP;
-    protocol.ai_flags = AI_PASSIVE;
-    char recievedMessage[512];
-    WSAResult = getaddrinfo(argv[1], DEFAULT_PORT, &protocol, &result);
-    
-    for (ptr = result;ptr != NULL;ptr = ptr -> ai_next){
-        ourSocket = socket(ptr -> ai_family,ptr -> ai_socktype,ptr ->ai_protocol);
-        if (ourSocket == INVALID_SOCKET){
-            printf("Socket failed");
-            WSACleanup();
-            return 1;
-        }
-       WSAResult = connect( ourSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-       if (WSAResult == SOCKET_ERROR){
-           printf("Socket failed");
-           WSACleanup();
-           return 1;
-       }
-       break; // connection acheived
-    }
-    freeaddrinfo(result);
-    
-    while(true){
-        std::cout<<"Enter To Send"<<std::endl;
-        char message[10];
-        std::cin>>message;
-        WSAResult = send(ourSocket,message,(int)strlen(message),0);
-        if(WSAResult == SOCKET_ERROR){
-            printf("Message Send Fail");
-            closesocket(ourSocket);
-            WSACleanup();
-            return 1;
-        }
-        printf("Message Sent");
-        break;
-        // build check to break out once user doesnt want to send no more messsages
-    }
-    WSAResult = shutdown(ourSocket,SD_SEND);
-    do{
-        WSAResult = recv(ourSocket, recievedMessage,512,0);
-        if (WSAResult > 0){
-            printf("Bytes received: %d\n",WSAResult);
-        }
-    }
-    while(WSAResult>0);
 
-    closesocket(ourSocket);
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    // Attempt to connect to an address until one succeeds
+    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+
+        // Create a SOCKET for connecting to server
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, 
+            ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 1;
+        }
+
+        // Connect to server.
+        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // Send an initial buffer
+
+    cout << "Input:";
+
+    string toSend;
+    cin >> toSend;
+    //toSend.replace(toSend.begin(), toSend.end(), ' ', '_');
+    cout << endl;
+    int len = toSend.length();
+    char char_array[512]; // this is the character limit
+    strcpy(char_array, toSend.c_str());
+
+    char *toSendFinal = char_array;
+
+    //const char *toSendFinal = "this is my string for memes";
+
+    iResult = send( ConnectSocket, toSendFinal, (int)strlen(toSendFinal), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    // shutdown the connection since no more data will be sent
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Receive until the peer closes the connection
+    do {
+
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if ( iResult > 0 )
+            printf("Bytes received: %d\n", iResult);
+        else if ( iResult == 0 )
+            printf("Connection closed\n");
+        else
+            printf("recv failed with error: %d\n", WSAGetLastError());
+
+    } while( iResult > 0 );
+
+    // cleanup
+    closesocket(ConnectSocket);
     WSACleanup();
 
-    
-
-
-
+    return 0;
 }
---
+++ src/Server/C++/server.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+// NOTES:
+// Currently works with Putty connection
+// Use RAW but Telnet works
+// TCP connection for when we need to build client
+// Compile with -std=c++0x -lws2_32
+
+int main()
+{
+	// Initialze winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return 0;
+	}
+	
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return 0;
+	}
+
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+	
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+	listen(listening, SOMAXCONN);
+
+	fd_set master; // Create the FD
+	FD_ZERO(&master); // Set the FD to zero
+	FD_SET(listening, &master); // Add the listening socket
+
+	// this will be changed by the \quit command (see below, bonus not in video!)
+	bool running = true; 
+
+	while (running)
+	{
+		fd_set copy = master; // Copy the list to maintain inactive conns - non active conns are not returned
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr); // Get active conns
+		for (int i = 0; i < socketCount; i++) // Loop through all the current & potential connections
+		{
+			SOCKET sock = copy.fd_array[i]; // Recieve data from the socket
+			if (sock == listening) // Check if connection request (aka new user)
+			{
+				SOCKET client = accept(listening, nullptr, nullptr); // Accept a new conn
+				FD_SET(client, &master); // Add new conn to FD
+				string welcomeMsg = "Welcome to the Game!\r\n"; // Define welcome message
+				send(client, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0); // Send welcome message
+			}
+			else // Its an inbound message
+			{
+				char buf[4096]; // Allocate memory for message
+				ZeroMemory(buf, 4096); // Zero the memory
+				int bytesIn = recv(sock, buf, 4096, 0); // Recieve the message (in bytes)
+				string clientText = string(buf, bytesIn); // Convert message to string
+				if (!clientText.empty() && clientText[clientText.length()-1] == '\n') { // Removes new line from message
+    				clientText.erase(clientText.length()-1);
+				}
+				if (bytesIn <= 0) // Checks if the response was blank - not blank unless disconnected
+				{
+					closesocket(sock); // Closes the socket connection
+					FD_CLR(sock, &master); // Removes the socket from FD
+				}
+				else if (clientText == "\n" || clientText == "\r" || clientText == " " || clientText ==  ""){ 
+					continue; // Ignores empty message
+				}
+				else {
+
+					cout << "Client: " + sock << endl;
+					cout << "Message: " + clientText << endl;
+					cout << "  " << endl;
+
+					for (int i = 0; i < master.fd_count; i++) // Sends message to all sockets 
+					{
+						SOCKET outSock = master.fd_array[i];
+						if (outSock != listening && outSock != sock) // Bypasses listening socket
+						{
+							ostringstream ss;
+							ss << "SOCKET #" << sock << ": " << buf << "\r\n";
+							string strOut = ss.str();
+
+							send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// If here, the server is shutting down
+
+	FD_CLR(listening, &master); // Removes listening socket
+	closesocket(listening); // Closes listening socket connection
+	
+	// Sends shutdown message, and disconnects sockets
+	string msg = "Server is shutting down. Goodbye\r\n";
+	while (master.fd_count > 0)
+	{
+		SOCKET sock = master.fd_array[0];
+		send(sock, msg.c_str(), msg.size() + 1, 0);
+		FD_CLR(sock, &master);
+		closesocket(sock);
+	}
+	WSACleanup(); // Cleanup winsock
+}
