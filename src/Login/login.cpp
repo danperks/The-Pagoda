0dbe8c883f10ceb1a12e81da1a812260d7fd365f
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-02-24 14:28:04 +0000

    Added login

--- src/Login/login.cpp
+++ src/Login/login.cpp
@@ -1,1 +1,46 @@
-// TODO
+#define SHA256 1+1;
+#define sha256 1+1;
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+// #include "sha256.h"
+
+using namespace std;
+
+
+int main()
+{
+    string name;
+    string password;
+    cout << "Enter username: ";
+    cin >> name;
+    cout << endl;
+    cout << "Enter password: ";
+    cin >> password;
+    cout << endl;
+
+    SHA256 sha256;
+
+    string check = name = "," + sha256(password);
+    
+    ifstream file;
+    file.open("user.txt");
+
+    ostringstream data;
+    data << file.rdbuf(); // reading data
+    string out = data.str();
+
+
+    if (out.find(check)) 
+        {
+            cout << "logged in";
+            return 1;
+        }
+    else
+        {
+            cout << "not a valid login";
+            return 0;
+        }
+}
