7ef5ddacce69167beac1afbfe120915804ca647a
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2020-03-02 14:48:25 +0000

    Update reciever.cpp

--- src/Components/GUI/Example/reciever.cpp
+++ src/Components/GUI/Example/reciever.cpp
@@ -2,3 +2,11 @@
 #include <Windows.h>
 #include <string>
 
+//set title as The Pagoda - Window
+
+int main(){
+    HWND foreground = GetActiveWindow();
+    SendMessage(foreground,WM_SETTEXT,0,(LPARAM)"The Pagoda - Window");
+    LPWSTR window_title;
+    GetWindowText(foreground,window_title,256);
+}
