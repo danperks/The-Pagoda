590bc4628e47b46b840759f88fbf7445e637025e
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2020-03-15 12:22:51 +0000

    read
    MainGame client.cpp has errors obvs but think theyre based off include errors with the crow, the bit i did doesnt have the same errors when doen without the crow. Will review ehen all compled.

--- Components/Server/C++/client - fakethread.cpp
+++ Components/Server/C++/client - fakethread.cpp
@@ -22,11 +22,13 @@ int main()
 	int a = 1;
         int gameID = 0;
         int userID=0;
+		string InitCommand =  "{\"gameID\":\"12345\",\"type\":\"command\",\"data\":\"INIT\",\"sender\":\"server\",\"recipient\":";// just for formtting
         string LockComamnd = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"quit\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
-        string VoteComamnd = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"vote\",\"sender\":\"server\",\"recipient\":ALL}";
-		string UnlockCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"unlock\",\"sender\":\"server\",\"recipient\":ALL}";
+        string VoteComamnd = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"vote\",\"sender\":\"server\",\"recipient\":\"ALL\"}";
+		string UnlockCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"unlock\",\"sender\":\"server\",\"recipient\":\"ALL\"}";
 		string KickedCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"kick\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
 		string WinnerCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"Winner\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
+		string WhisperCommand =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"whisper\",\"data\":\"message\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
 	//string PerksClient ="C:\\Users\\hpd12\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
 	string address = "127.0.0.1";		// IP Address of the server
 	int port = 54000;					// Listening port # on the server
@@ -83,12 +85,30 @@ int main()
 		letter = "";                    
     	word = "";    
 		letterCount = 0;
+		int EstablishConnection = recv(sock,buffer,4096,0);//gets the details from the first init comand and sets them
+		string initDetails = string(buffer,0,EstablishConnection);
+		//cehcks  its receiving something containg a inti command - this is open for abuse but its gettting on a bit , so can be a low priority review?
+			userID = stoi(initDetails.substr(initDetails.find("recipient\":")+1));
+			unsigned delimit1 = initDetails.find("gameID\":")+1;
+			unsigned delimit2 = initDetails.find("\",\"type");
+			gameID = stoi(initDetails.substr(delimit1,delimit2-delimit1));
+
 
 		while(true){
 			if (_kbhit() ) {
 				cout<<"Type Here"<<endl;
+				cout<<"For Whisper type / whispher and recipient etc(whisper 234) else , just type message to all / command previosuly given"<<endl;
+				
 				string userentry;
 				cin >>userentry;
+				if(userentry.find("whisper")){
+					string recipient = userentry.substr(userentry.find("whisper")+1);
+					string userentry =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"whisper\",\"data\":\""+userentry+"\",\"sender\":\"server\",\"recipient\":"+recipient+"}";
+				}
+				else{
+					string userentry =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"broadcast\",\"data\":\""+userentry+"\",\"sender\":\"server\",\"recipient\":"+"ALL"+"}"; 
+				}
+				
 				toSend.push_back(userentry);
 			}
 			else {
@@ -96,12 +116,14 @@ int main()
 				if (dataOut > 0)
 				{
 					cout<<"SERVER> " + string(buffer, 0, dataOut)<<endl;
+					 if (string(buffer,0,dataOut).find("recipient\":"+to_string(userID)) || string(buffer,0,dataOut).find("recipient\": ALL")){
+						 //checks if directed at you or all , those not will jsut straight up be ignored
 					 if(LockComamnd == string(buffer, 0, dataOut)){
 						                                // go over this again
                                 clientUnlocked == false;
 								cout<<"Console Locked , Vote Recieved"<<endl;
 								while(clientUnlocked == false){
-									//waiting for unlock command;
+									//waiting for unlock command; // cant send anything untill unlock command recieved
 									int dataOut2 = recv(sock,buffer,4096,0);
 									if(UnlockCommand == string(buffer,0,dataOut2)){
 										clientUnlocked == true; // client then unlocked
@@ -125,6 +147,7 @@ int main()
 				}
 			}
 		}
+		}
 		
 		int sendResult = send(sock, toSend.front().c_str(), toSend.front.c_str() + 1, 0);
 	}
@@ -133,3 +156,4 @@ int main()
 	closesocket(sock);
 	WSACleanup();
 }
+	}
--- Main Game/client/client.cpp
+++ Main Game/client/client.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <ctype.h>
 #include <windows.h>
+#include <iostream>
+#include <winsock2.h>
 
 // include the path with crow_all.h in
 // include Boost lib
@@ -13,7 +15,9 @@
 #define BOOST_THREAD_USE_LIB
 
 #define _WIN32_WINNT_WIN10                  0x0A00
-
+#define WIN32_LEAN_AND_MEAN
+#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
 using namespace std;
 
 class Network { // For sending and recieving data
@@ -89,8 +93,13 @@ class Game { // For running the game logic
 	    int a = 1;
         int gameID = 0;
         int userID=0;
-        string LockComamnd = "{\"gameID\":"+gameID+",\"type\":\"command\",\"data\":\"quit\",\"sender\":\"server\",\"recipient\":"+userID+"}";
-        string VoteComamnd = "{\"gameID\":"+gameID+",\"type\":\"command\",\"data\":\"vote\",\"sender\":\"server\",\"recipient\":"+userID+"}";
+		string InitCommand =  "{\"gameID\":\"12345\",\"type\":\"command\",\"data\":\"INIT\",\"sender\":\"server\",\"recipient\":";// just for formtting
+        string LockComamnd = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"quit\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
+        string VoteComamnd = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"vote\",\"sender\":\"server\",\"recipient\":\"ALL\"}";
+		string UnlockCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"unlock\",\"sender\":\"server\",\"recipient\":\"ALL\"}";
+		string KickedCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"kick\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
+		string WinnerCommand = "{\"gameID\":"+to_string(gameID)+",\"type\":\"command\",\"data\":\"Winner\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
+		string WhisperCommand =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"whisper\",\"data\":\"message\",\"sender\":\"server\",\"recipient\":"+to_string(userID)+"}";
 
 
 
@@ -129,47 +138,89 @@ class Game { // For running the game logic
                 return 0;
             vector<string> toSend;
             string letter = "";                
-            string word = "";    
+            string word = "";
+            string dataIn;
+            char buffer[4096];    
 	        int letterCount = 0;
             bool clientUnlocked = true;
 
 
 	        while(true){
-                toSend.clear();
-                letter = "";
-                word = "";    
-		        letterCount = 0;
-                while(true){
-                    if (_kbhit() ) {
-                        int key_code = _getch();
-                        string key = to_string(key_code);
-                        if (key == "13"){
-                            break;
-                        }
-                        printf("The character is %c\n", key);
-                        toSend.push_back(key);
-                    }
-			        else {
-                        int dataOut = recv(sock, buffer, 4096, 0);
-                        if (dataOut > 0)
-                        {
-                            printf("SERVER> " + string(buffer, 0, dataOut));
-                           
-                            if(VoteComand == string(buffer, 0, dataOut)){
-                                // go over this again
-                                clientUnlocked == false;
+		toSend.clear();
+		letter = "";                    
+    	word = "";    
+		letterCount = 0;
+		int EstablishConnection = recv(sock,buffer,4096,0);//gets the details from the first init comand and sets them
+		string initDetails = string(buffer,0,EstablishConnection);
+		//cehcks  its receiving something containg a inti command - this is open for abuse but its gettting on a bit , so can be a low priority review?
+			userID = stoi(initDetails.substr(initDetails.find("recipient\":")+1));
+			unsigned delimit1 = initDetails.find("gameID\":")+1;
+			unsigned delimit2 = initDetails.find("\",\"type");
+			gameID = stoi(initDetails.substr(delimit1,delimit2-delimit1));
 
-                                cout<<"Console Locked , Vote Recieved"<<endl;
-                        }
-		    	}   
-		    }
+
+		while(true){
+			if (_kbhit() ) {
+				cout<<"Type Here"<<endl;
+				cout<<"For Whisper type / whispher and recipient etc(whisper 234) else , just type message to all / command previosuly given"<<endl;
+				
+				string userentry;
+				cin >>userentry;
+				if(userentry.find("whisper")){
+					string recipient = userentry.substr(userentry.find("whisper")+1);
+					string userentry =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"whisper\",\"data\":\""+userentry+"\",\"sender\":\"server\",\"recipient\":"+recipient+"}";
+				}
+				else{
+					string userentry =  "{\"gameID\":"+to_string(gameID)+",\"type\":\"broadcast\",\"data\":\""+userentry+"\",\"sender\":\"server\",\"recipient\":"+"ALL"+"}"; 
+				}
+				
+				toSend.push_back(userentry);
+			}
+			else {
+				int dataOut = recv(sock, buffer, 4096, 0);
+				if (dataOut > 0)
+				{
+					cout<<"SERVER> " + string(buffer, 0, dataOut)<<endl;
+					 if (string(buffer,0,dataOut).find("recipient\":"+to_string(userID)) || string(buffer,0,dataOut).find("recipient\": ALL")){
+						 //checks if directed at you or all , those not will jsut straight up be ignored
+					 if(LockComamnd == string(buffer, 0, dataOut)){
+						                                // go over this again
+                                clientUnlocked == false;
+								cout<<"Console Locked , Vote Recieved"<<endl;
+								while(clientUnlocked == false){
+									//waiting for unlock command; // cant send anything untill unlock command recieved
+									int dataOut2 = recv(sock,buffer,4096,0);
+									if(UnlockCommand == string(buffer,0,dataOut2)){
+										clientUnlocked == true; // client then unlocked
+									}
+								}
+
+                                
+				}
+				if(KickedCommand == string(buffer,0,dataOut)){
+					cout<<"You are no longer a contestant to win the game. Thank You. Come Again."<<endl;
+					cout<<"You will be forcefully disconnected. You have no choice."<<endl;
+					
+					break;
+				}
+				if(WinnerCommand == string(buffer,0,dataOut)){
+					cout<<"You have played well. You are the winner. You should be ashamed. Thank You . Come Again."<<endl;
+					cout<<"You will now be forcefully disconnected. You have no choice"<<endl;
+					
+					break;
+					
+				}
+			}
+		}
+		}
 		
-		int sendResult = send(sock, dataIn.c_str(), dataIn.size() + 1, 0);
+		int sendResult = send(sock, toSend.front().c_str(), toSend.front.c_str() + 1, 0);
 	}
 
 
 	closesocket(sock);
 	WSACleanup();
+}
 };
 
 
